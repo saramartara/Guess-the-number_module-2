74d58c89ccb20f916baa7703368fa59b79ab200c
saramartara <saramartinezaragon@gmail.com>
2021-01-28 11:09:05 +0100

    last commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 'use strict';
-console.log('vamos');
 
+// cuando se recarga la página se genera un número aleatorio entre 1-100 y se muestra en consola.
+// += se muestra en .js-clue Pista: Escribe un número y dale a Prueba
 const clueElement = document.querySelector('.js-clue');
 
 function getRandomNumber(max) {
   clueElement.value = 'Pista: Escribe el número y dale a Prueba';
   return Math.ceil(Math.random() * max);
 }
-console.log(getRandomNumber(100));
+
+let randomNumber = getRandomNumber(100);
+console.log(randomElement);
+
+// let randomNumber = getRandomNumber(100);
+// console.log(randomNumber);
 
 window.addEventListener('load', getRandomNumber);
-// cuando se recarga la página se genera un número aleatorio entre 1-100 y se muestra en consola.
-// += se muestra en .js-clue Pista: Escribe un número y dale a Prueba
+
+// CUANDO la usuaria clika el botón de prueba, recoger el número escrito en '.js-inputNumber', regogerlo y compararlo con RandomNumber.
+
+const buttonElement = document.querySelector('.js-bnt')
+const NumberElement = document.querySelector('.js-inputNumber');
+
+function handleComparation{
+    let NumberValue = NumberElement.value;
+    if (NumberValue === randomNumber) {
+
+    }
+    
+}
+
+buttonElement.addEventListener('click', handleComparation);
