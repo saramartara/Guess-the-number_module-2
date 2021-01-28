a0f06c9d5f9fd8b450902855b8f7467c28c71743
saramartara <saramartinezaragon@gmail.com>
2021-01-28 10:42:05 +0100

    add function to get random number and clue messege

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
 console.log('vamos');
+
+const clueElement = document.querySelector('.js-clue');
+
+function getRandomNumber(max) {
+  clueElement.value = 'Pista: Escribe el número y dale a Prueba';
+  return Math.ceil(Math.random() * max);
+}
+console.log(getRandomNumber(100));
+
+window.addEventListener('load', getRandomNumber);
+// cuando se recarga la página se genera un número aleatorio entre 1-100 y se muestra en consola.
+// += se muestra en .js-clue Pista: Escribe un número y dale a Prueba
