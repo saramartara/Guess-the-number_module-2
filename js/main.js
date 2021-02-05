01c939b125b462d691fca8bf9b3fcf361094c340
saramartara <saramartinezaragon@gmail.com>
2021-02-05 22:41:17 +0100

    add counter and render data

--- js/main.js
+++ js/main.js
@@ -1,51 +1,45 @@
 'use strict';
 
-//Data:
-// número usuaria
-// número aleatorio
-//si recojo el value del input al cargar la página, estará vacío
-// tengo que recogerlo cuando la usuaria haga click
-
 const numberInput = document.querySelector('.js-number');
 const testButton = document.querySelector('.js-button');
 const tipElement = document.querySelector('.js-clue');
-console.log(tipElement);
-// const userNumberValue = numberInput.value;
+const counterElement = document.querySelector('.js-tries');
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
 function renderTip(message) {
-  tipElement.innerHTML = message;
+  tipElement.innerHTML = 'Pista: ' + message;
 }
 
-function handleClickTestButton(event) {
-  //steps:
-  //1. take user number
-  //2. check if it is higher
-  //3. check if it is lower
-  //4. check if it is equal
-  //5.check number is in range
-  //6. Render message
-
+function checkNumber() {
   const userNumberValue = numberInput.value;
-  const numberValue = parseInt(userNumberValue); // ojo! parseInt para que pueda comparar número con número
+  const numberValue = parseInt(userNumberValue);
 
-  if (userNumberValue === '') {
+  if (numberValue === '') {
     renderTip('El número debe estar entre 1 y 100');
-  } else if (userNumberValue < 1 || userNumberValue > 100) {
+  } else if (numberValue < 1 || numberValue > 100) {
     renderTip('El número debe estar entre 1 y 100');
-  } else if (userNumberValue > randomNumber) {
+  } else if (numberValue > randomNumber) {
     renderTip('Demasiado alto');
-  } else if (userNumberValue < randomNumber) {
+  } else if (numberValue < randomNumber) {
     renderTip('Demasiado bajo');
-  } else if (userNumberValue === randomNumber) {
+  } else {
     renderTip('¡Has ganado, campeona!');
   }
 }
+let triesCounter = 0;
+function increaseCounter() {
+  triesCounter = triesCounter + 1;
+  counterElement.value = `Numero de intentos: ${triesCounter}`;
+}
+
+function handleClickTestButton() {
+  checkNumber();
+  increaseCounter();
+}
 
 const randomNumber = getRandomNumber(100);
-console.log(randomNumber);
 
 testButton.addEventListener('click', handleClickTestButton);
